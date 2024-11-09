012b6bbef84b80b51605ef5985904113f58a17d7
anjanavu <anjanavu2000@gmail.com>
2024-11-09 17:00:01 +0530

    #compress

--- src/utils/s3Upload.js
+++ src/utils/s3Upload.js
@@ -1,4 +1,5 @@
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import imageCompression from "browser-image-compression";
 
 const uploadFileToS3 = async (file, onSuccess, onError) => {
   const S3_BUCKET = import.meta.env.VITE_APP_AWS_S3_BUCKET;
@@ -14,16 +15,31 @@ const uploadFileToS3 = async (file, onSuccess, onError) => {
     },
   });
 
-  const params = {
-    Bucket: S3_BUCKET,
-    Key: file.name,
-    Body: file,
-  };
-
   try {
+    let fileToUpload = file;
+
+    // Check if file size is greater than 1MB (1MB = 1 * 1024 * 1024 bytes)
+    if (file.size > 1 * 1024 * 1024 && file.type.startsWith("image/")) {
+      console.log("Compressing image...");
+      const options = {
+        maxSizeMB: 1, // Target size in MB
+        maxWidthOrHeight: 1920, // Optional: Resize image to reduce file size further
+        useWebWorker: true,
+      };
+      fileToUpload = await imageCompression(file, options);
+      console.log("Image compressed successfully");
+    }
+
+    const params = {
+      Bucket: S3_BUCKET,
+      Key: fileToUpload.name,
+      Body: fileToUpload,
+    };
+
     const command = new PutObjectCommand(params);
     const data = await s3Client.send(command);
-    const location = `https://${S3_BUCKET}.s3.${REGION}.amazonaws.com/${file.name}`;
+
+    const location = `https://${S3_BUCKET}.s3.${REGION}.amazonaws.com/${fileToUpload.name}`;
     if (onSuccess) onSuccess(location);
   } catch (err) {
     console.error("Error uploading file:", err);
