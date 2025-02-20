eb9ae3baf150877f3dd77892af719a7e64464284
Anjana V U <103420152+anjanavu@users.noreply.github.com>
2025-02-20 22:26:21 +0530

    #event

--- src/components/Event/AddEvent.jsx
+++ src/components/Event/AddEvent.jsx
@@ -105,7 +105,7 @@ export default function AddEvent({ setSelectedTab, isUpdate }) {
     if (isUpdate) {
       navigate("/events/list");
     }
-    setSelectedTab(0);
+    // setSelectedTab(0);
 
     reset();
   };
